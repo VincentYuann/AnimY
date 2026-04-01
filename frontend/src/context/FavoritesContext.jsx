2dc6d09281585c7934f0c09fe2fad84355e836dd
Vincent Yuan <vincentyuan1020@gmail.com>
2026-04-01 17:02:45 -0400

    deleted some line

--- frontend/src/context/FavoritesContext.jsx
+++ frontend/src/context/FavoritesContext.jsx
@@ -1,6 +1,6 @@
 import { useState, useContext, createContext, useEffect } from "react";
 import { useAuth } from "../context/AuthContext";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+import { useMutation, useQuery } from "@tanstack/react-query"
 import favoritesService from "../services/favoritesService";
 import { toast } from "react-hot-toast";
 
@@ -8,7 +8,6 @@ const favoritesContext = createContext();
 
 function favoritesProvider({ children }) {
     const { user } = useAuth();
-    const queryClient = useQueryClient();
 
     const { data: favorites = [] } = useQuery({
         queryKey: ['favorites', user?.id],
